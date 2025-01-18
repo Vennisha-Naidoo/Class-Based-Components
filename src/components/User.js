ba55261e9a6767d6e7e4891153a555b76a447c9c
Vennisha-Naidoo <vennisha31@gmail.com>
2025-01-18 02:08:19 +0200

    first commit

---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import classes from './User.module.css';
+
+//Class-based Component
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+//Functional Component
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
