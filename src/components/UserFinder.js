3dd3b46f86d137d4296f1cb6baf276c84aa86903
Vennisha-Naidoo <vennisha31@gmail.com>
2025-01-24 17:14:52 +0200

    Error Boundaries - for Class-based components

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,8 +1,8 @@
 import { Fragment, useState, useEffect, Component } from 'react';
-
 import Users from './Users';
 import classes from './UserFinder.module.css';
 import UsersContext from '../store/users-context';
+import ErrorBoundary from './ErrorBoundary';
 
 //Class-based Component
 class UserFinder extends Component {
@@ -39,7 +39,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type='search' onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
